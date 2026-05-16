d71dbda3388ab1677bf8d79839b6d44eaa8aac13
Boris <stepanenkoboris064@gmail.com>
2026-05-16 18:33:38 +0300

    мигрировал базу данных. на сервере просто npx payload migrate

---
+++ backend/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260516_152643 from './20260516_152643';
+
+export const migrations = [
+  {
+    up: migration_20260516_152643.up,
+    down: migration_20260516_152643.down,
+    name: '20260516_152643'
+  },
+];
