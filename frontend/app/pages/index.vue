81c3159bda3a457897b20d7cfb8860e2377a8e3f
Boris <stepanenkoboris064@gmail.com>
2026-05-16 16:58:38 +0300

    feat: мажорное обновление структуры проекта и фронтенда
      - Отключен SSR, настроен SPA-режим с очисткой маршрутов для пререндеринга
      - Добавлены модули: @pinia/nuxt, @nuxt/ui, @vueuse/nuxt, nuxt-single-html
      - Обновлен nuxt.config.ts: настройки заголовков, meta-тегов, runtimeConfig
    
      - Добавлены UI/стилизация: @nuxt/ui, tailwindcss, tailwind-merge, tailwind-variants, @vueuse/nuxt
      - Добавлены state-менеджмент: pinia, @pinia/nuxt,
      - Добавлена валидация: zod
      - Добавлена анимация: @formkit/auto-animate
      - Фиксированы версии: vue@3.5.27, vue-router@4.6.4, nuxt@4.3.0
      - Добавлены dev-зависимости: typescript, @types/node, nuxt-single-html

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-6xl text-center mt-20">Если ты это видишь, то Nuxt робит</h1>
+</template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,62 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  spaLoadingTemplate: true,
+  hooks: {
+    "prerender:routes"({ routes }) {
+      routes.clear() //Do not generate any routes
+    }
+  },
+  //для сервера вроде не нужно
+  // router: {
+  //   options: {
+  //     hashMode: true,
+  //   }
+  // },
+  css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      API_URL: process.env.API_URL,
+    }
+  },
+  app: {
+    head: {
+      title: 'FreshCheck',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://awesome-lib.css' },
+        //лого сюда
+        // { rel: 'icon', type: 'image/svg', href: '/logo.svg' },
+      ],
+    },
+  },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: {
+    enabled: false,
+    timeline: {
+      enabled: true,
+    },
+  },
+  modules: ['@pinia/nuxt', '@nuxt/ui', 'nuxt-single-html', '@vueuse/nuxt'],
+  
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+  pinia: {
+    /**
+     * Automatically add stores dirs to the auto imports. This is the same as
+     * directly adding the dirs to the `imports.dirs` option. If you want to
+     * also import nested stores, you can use the glob pattern `./stores/**`
+     * (on Nimport { API_URL } from './app/utils/constants';
+uxt 3) or `app/stores/**` (on Nuxt 4+)
+     *
+     * @default `['stores']`
+     */
+    storesDirs: []
+  },
 })
