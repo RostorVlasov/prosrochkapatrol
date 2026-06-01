c9729bf93e7c34a80c018e70ebc997612f9dc1d1
Boris Stepanenko <stepanenkoboris064@gmail.com>
2026-06-01 07:41:05 +0300

    Update DocsFaq.vue

--- frontend/app/components/docs/sections/DocsFaq.vue
+++ frontend/app/components/docs/sections/DocsFaq.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="py-24">
+    <section id="faq" class="py-24">
         <div class="max-w-4xl mx-auto">
             <FaqSection pill="Правовые вопросы" title-before="Юридическая" title-italic="чистота"
                 subtitle="Ответы на ключевые вопросы о законности общественного мониторинга" :items="legalFaq" />
