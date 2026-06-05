ac8ef2ec375074ca7e670e1ecbdb78c772acca69
Boris <stepanenkoboris064@gmail.com>
2026-06-06 01:24:32 +0300

    fix server url

--- backend/src/payload.config.ts
+++ backend/src/payload.config.ts
@@ -23,7 +23,7 @@ const dirname = path.dirname(filename)
 
 export default buildConfig({
   globals: [SiteSettings],
-  serverURL: 'https://api.FreshCheckAstra.ru',
+  serverURL: 'https://api.freshcheckastra.ru',
   cors: [
     'http://localhost:3000', 
     'https://FreshCheckAstra.ru',
