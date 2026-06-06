d1e57bd194f6ed6698665db29c01ebb13bd26176
Boris <stepanenkoboris064@gmail.com>
2026-06-06 16:19:15 +0300

    fix bug delete logs

--- frontend/app/composables/usePushNotifications.ts
+++ frontend/app/composables/usePushNotifications.ts
@@ -1,5 +1,3 @@
-// composables/usePushNotifications.ts
-
 function urlBase64ToUint8Array(base64String: string) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
@@ -9,42 +7,30 @@ function urlBase64ToUint8Array(base64String: string) {
 
 export const usePushNotifications = () => {
   const subscribe = async () => {
-    console.log(navigator)
-    if (!('serviceWorker' in navigator) || !('PushManager' in window)) return
-    console.log('Проверка пройдена');
-    
-    const reg = await navigator.serviceWorker.register('/sw.js')
-    console.log(reg)
-    console.log('Регистрация прошла успешно')
-    const permission = await Notification.requestPermission()
-    console.log('Запрос подтверждение');
-    if (permission !== 'granted') return
-    console.log('Уведомления одобрены');
+  if (!('serviceWorker' in navigator) || !('PushManager' in window)) return
 
-    console.log(useRuntimeConfig().public.vapidPublicKey)
-    console.log('Публичный ключ')
-    const sub = await reg.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(
-        useRuntimeConfig().public.vapidPublicKey as string
-      )
-    })
+  const reg = await navigator.serviceWorker.register('/sw.js')
+  
+  const existingSub = await reg.pushManager.getSubscription()
+  if (existingSub) return
+  
+  const permission = await Notification.requestPermission()
+  if (permission !== 'granted') return
 
+  const sub = await reg.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(
+      useRuntimeConfig().public.vapidPublicKey as string
+    )
+  })
 
-    console.log('Подписка получена')
-    console.log(sub)
+  const { endpoint, keys } = sub.toJSON()
 
-    const { endpoint, keys } = sub.toJSON()
-    console.log(endpoint, keys)
-    await $fetch('https://api.freshcheckastra.ru/api/push-subscriptions', {
-      method: 'POST',
-      body: { endpoint, keys }
-    })
-    
-    console.log('Ключи отправлены')
-    console.log({ endpoint, keys })
-
-  }
+  await $fetch('https://api.freshcheckastra.ru/api/push-subscriptions', {
+    method: 'POST',
+    body: { endpoint, keys }
+  })
+}
 
   return { subscribe }
 }
