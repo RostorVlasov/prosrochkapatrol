5e5c08e04576a6c9df49ee12ed546c423ddbd8c3
MerVik <stepanenkoboris064@gmail.com>
2026-07-10 11:08:16 +0300

    убран блок главного партнера

--- frontend/app/components/partners/PartnersView.vue
+++ frontend/app/components/partners/PartnersView.vue
@@ -1,10 +1,6 @@
 <template>
     <LandingContainer>
         <PartnersHero />
-
-        <div class="mb-12">
-            <MainPartner v-bind="mainPartnerBase" :logo="logo"/>
-        </div>
         <section class="max-w-7xl mx-auto pb-24">
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
                 <PartnerCard v-for="(partner, index) in partners" :key="index" v-bind="partner" />
@@ -17,20 +13,7 @@
 </template>
 
 <script setup lang="ts">
-import { mainPartnerBase, partners } from '~/data/data'
-
-const mode = useColorMode()
-const isDark = ref(false)
-
-onMounted(() => {
-  isDark.value = mode.value === 'dark'
-
-  watch(() => mode.value, (val) => {
-    isDark.value = val === 'dark'
-  })
-})
-
-const logo = computed(() => isDark.value ? '/news_people_white.png' : '/news_people_black.png')
+import { partners } from '~/data/data'
 
 useSeoMeta({
     title: 'Партнеры FreshCheck',
